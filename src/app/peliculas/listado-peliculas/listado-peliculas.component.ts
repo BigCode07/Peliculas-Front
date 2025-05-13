585b263c859a6f65bd559dca9dd1a64aa24fd1d3
bigcode <bigcode07@gmail.com>
2025-05-12 21:37:54 -0300

    feat: Creacion de eventos

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, model } from '@angular/core';
 
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
@@ -8,5 +8,30 @@ import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
   templateUrl: './listado-peliculas.component.html',
 })
 export class ListadoPeliculasComponent {
-  peliculas = input<any[]>([]);
+  peliculas = model<any[]>([]);
+
+  agregarPelicula() {
+    this.peliculas.update((v) => [
+      ...v,
+      {
+        id: Date.now(),
+        titulo: 'Inception',
+        fechaLanzamiento: new Date('2012-07-03'),
+      },
+    ]);
+  }
+
+  remover(pelicula: any) {
+    const indice = this.peliculas().findIndex(
+      (peliActual: any) => peliActual.id === pelicula.id
+    );
+
+    if (indice !== -1) {
+      this.peliculas.update((v) => {
+        const nuevoArray = [...v];
+        nuevoArray.splice(indice, 1); // <-- aquí está el cambio
+        return nuevoArray;
+      });
+    }
+  }
 }
